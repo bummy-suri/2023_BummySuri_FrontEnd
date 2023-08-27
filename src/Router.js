2bdb940e28e1e36414f510d7ab643bd554b68ffe
MiraeKim <hufs202000512@gmail.com>
2023-08-28 00:45:40 +0900

    feat: 가위바위보게임 구현

--- src/Router.js
+++ src/Router.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Lending from "./pages/Lending";
 import Login from "./pages/Login";
 import Minting from "./pages/Minting";
+import Rps from "./pages/Game/Rps";
 
 
 function DefaultRouter () {
@@ -12,6 +13,7 @@ function DefaultRouter () {
           <Route path="/" element={<Lending />} />
           <Route path="/Login" element={<Login />} />
           <Route path="/Minting" element={<Minting />} />
+          <Route path="/rock-paper-scissors" element={<Rps />} />
         </Routes>
     </Router>
     );

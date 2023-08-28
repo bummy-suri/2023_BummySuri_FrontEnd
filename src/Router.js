45ff7d66517fda9289e25be915ee884a0d1ded34
choihaha123 <ensuni123@naver.com>
2023-08-28 20:21:15 +0900

    Design: 하미플 부분 디자인 수정

--- src/Router.js
+++ src/Router.js
@@ -5,8 +5,6 @@ import Login from "./pages/Login";
 import Minting from "./pages/Minting";
 import Rps from "./pages/Game/Rps";
 import RpsResult from "./pages/Game/RpsResult";
-import Betting from "./pages/BettingGame/Betting";
-import Predicting from "./pages/BettingGame/Predicting";
 
 
 function DefaultRouter () {
@@ -18,9 +16,6 @@ function DefaultRouter () {
           <Route path="/Minting" element={<Minting />} />
           <Route path="/rock-paper-scissors" element={<Rps />} />
           <Route path="/rock-paper-scissors/result" element={<RpsResult />} />
-
-          <Route path="/betting" element={<Betting/>} />
-          <Route path="/betting/predict" element={<Predicting/>}/>
         </Routes>
     </Router>
     );

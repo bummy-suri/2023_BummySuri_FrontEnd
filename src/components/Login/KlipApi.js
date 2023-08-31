9562afbeed2432de3a550ede56a0b18ee53bc7c3
MiraeKim <hufs202000512@gmail.com>
2023-08-31 19:21:44 +0900

    err

--- src/components/Login/KlipApi.js
+++ src/components/Login/KlipApi.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useCallback } from "react";
+import React, { useState, useRef } from "react";
 
 import styled from 'styled-components';
 import QRcode from "qrcode.react";
@@ -52,26 +52,9 @@ const KlipBtn = () => {
   const [qrvalue_auth, setQrvalue_auth] = useState(DEFAULT_QR_CODE);
   const walletAddress = useRef('');
 
- 
-      
-
-  const sendRequestKey = useCallback((request_key) => {
-    axios
-      .post(SendRequestKey_URL, {
-        requestKey: request_key,
-      })
-      .then(response => {
-        const { accessToken } = response.data;
-        localStorage.setItem("accessToken", accessToken);
-        console.log("accessToken", accessToken);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  }, []);
-
-  const getUserData = useCallback(() => {
-    localStorage.clear();
+  const getUserData = (setQrvalue, callback) => {
+    localStorage.clear()
+    // Prepare
     axios
       .post(A2P_API_PREPARE_URL, {
         bapp: {
@@ -84,18 +67,47 @@ const KlipBtn = () => {
         console.log(request_key);
         localStorage.setItem("requestKey", request_key);
 
+        // Request
         if (isMobile) {
           window.location.href = getKlipAccessUrl("deeplink", request_key);
         } else {
           setQrvalue_auth(getKlipAccessUrl("QR", request_key));
         }
 
+        
+
+
         sendRequestKey(request_key);
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+
+        
+
+  };
+      
+
+  const sendRequestKey = (request_key) => {
+    axios
+      .post('https://api.dev.bummysuri.com/users', {
+        requestKey: request_key,
+      })
+      .then(response => {
+        
+        const { accessToken } = response.data;
+        localStorage.setItem("accessToken", accessToken);
+        console.log("accessToken", accessToken);
       })
       .catch((error) => {
-        console.error(error);
+        if (error.response) {
+          console.error("응답 오류:", error.response.status, error.response.data);
+        } else {
+          console.error("요청 오류:", error.message);
+        }
       });
-  }, [sendRequestKey]);
+  };
+
 
 
 
